925f773d6c866237f5fa72e73ea4561c7329f23c
hafizbilal112 <hafizbilal112@gmail.com>
2015-05-05 07:57:47 +0500

    linked all accounts together and implemented unlink feature

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,19 @@
 //routes.js
 
 module.exports = function (app, passport) {
+    //basic routes
     app.get('/', function (req, res) {
         res.render('index.ejs');
     });
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {user: req.user});
+    });
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    //auth
     app.get('/login', function (req, res) {
         res.render('login.ejs', {message: req.flash('loginMessage')});
     });
@@ -15,14 +25,19 @@ module.exports = function (app, passport) {
     app.get('/signup', function (req, res) {
         res.render('signup.ejs', {message: req.flash('signupMessage')});
     });
-    app.post('/signup', passport.authenticate('local-register', {
+    app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/profile',
         failureRedirect: '/signup',
         failureFlash: true
     }));
-    app.get('/profile', function (req, res) {
-        res.render('profile.ejs', {user: req.user});
+    app.get('/login', function (req, res) {
+        res.render('login.ejs', {message: req.flash('loginMessage')});
     });
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
     app.get('/auth/facebook', passport.authenticate('facebook', {
         scope: 'email'
     }));
@@ -30,9 +45,39 @@ module.exports = function (app, passport) {
         successRedirect: '/profile',
         failureRedirect: '/'
     }));
-    app.get('/logout', function (req, res) {
-        req.logout();
-        res.redirect('/');
+
+    //connect
+    app.get('/connect/local', function (req, res) {
+        res.render('connect-local.ejs', {message: req.flash('loginMessage')});
+    });
+    app.post('/connect/local', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/connect/local',
+        failureFlash: true
+    }));
+    app.get('/connect/facebook', passport.authenticate('facebook', {
+        scope: 'email'
+    }));
+    app.get('/connect/facebook/callback', passport.authenticate('facebook', {
+        successRedirect: '/profile',
+        failureRedirect: '/'
+    }));
+
+    //unlink
+    app.get('/unlink/local', function (req, res) {
+        var user = req.user;
+        user.local.email = undefined;
+        user.local.password = undefined;
+        user.save(function (err) {
+            res.redirect('/profile');
+        });
+    });
+    app.get('/unlink/facebook', function (req, res) {
+        var user = req.user;
+        user.facebook.token = undefined;
+        user.save(function (err) {
+            res.redirect('/profile');
+        });
     });
 };
 
--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ module.exports = function (passport) {
             done(err, user);
         })
     });
-    passport.use('local-register', new LocalStrategy({
+    passport.use('local-signup', new LocalStrategy({
             usernameField: 'email',
             passwordField: 'password',
             passReqToCallback: true
@@ -61,31 +61,49 @@ module.exports = function (passport) {
             })
         }));
     passport.use(new FacebookStrategy({
-        clientID: configAuth.facebookAuth.clientID,
-        clientSecret: configAuth.facebookAuth.clientSecret,
-        callbackURL: configAuth.facebookAuth.callbackURL
-    },
-        function (token, refreshToken, profile, done) {
-            User.findOne({'facebook.id':profile.id}, function (err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (user) {
-                    return done(null, user);
-                }
-                else{
-                    var newUser = new User();
-                    newUser.facebook.id = profile.id;
-                    newUser.facebook.token = token;
-                    newUser.facebook.name = profile.name.givenName;
-                    newUser.facebook.email = profile.emails[0].value;
-                    newUser.save(function (err) {
+            clientID: configAuth.facebookAuth.clientID,
+            clientSecret: configAuth.facebookAuth.clientSecret,
+            callbackURL: configAuth.facebookAuth.callbackURL,
+            passReqToCallback: true
+        },
+        function (req, token, refreshToken, profile, done) {
+            process.nextTick(function () {
+                if (!req.user) {
+                    User.findOne({'facebook.id': profile.id}, function (err, user) {
+                        if (err) {
+                            return done(err);
+                        }
+                        if (user) {
+                            return done(null, user);
+                        }
+                        else {
+                            var newUser = new User();
+                            newUser.facebook.id = profile.id;
+                            newUser.facebook.token = token;
+                            newUser.facebook.name = profile.name.givenName;
+                            newUser.facebook.email = profile.emails[0].value;
+                            newUser.save(function (err) {
+                                if (err) {
+                                    throw err;
+                                }
+                                return done(null, newUser);
+                            })
+                        }
+                    });
+                } else {
+                    var user = req.user;
+                    user.facebook.id = profile.id;
+                    user.facebook.token = token;
+                    user.facebook.name = profile.name.givenName;
+                    user.facebook.email = profile.emails[0].value;
+                    user.save(function (err) {
                         if (err) {
                             throw err;
                         }
-                        return done(null, newUser);
+                        return done(null, user);
                     })
                 }
-            })
+
+            });
         }))
 };
